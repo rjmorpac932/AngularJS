6510d31e0b5eff4e0105b1856c79f67ac6efe36c
jmarcan286 <jmarcan286@iesalmudeyne.es>
2024-01-17 14:12:48 +0100

    :rocket: [UPGRADE] Nuevos Estilo de los Elementos Details

--- src/app/btn-modo/btn-modo.component.ts
+++ src/app/btn-modo/btn-modo.component.ts
@@ -19,10 +19,43 @@ export class BtnModoComponent {
   modoHandler(){
 
     if(this.modo === this.tipoModo.NOCHE){
+
       document.documentElement.style.colorScheme = "light";
+
+      document.querySelectorAll("details").forEach(detail => {
+        if(detail instanceof HTMLDetailsElement){
+          detail.style.backgroundColor = "#CACACA";
+        }
+      });
+      
+      document.querySelectorAll(".summary-chevron-down, .summary-chevron-up").forEach(flecha => {
+
+        if(flecha instanceof HTMLElement){
+          flecha.style.backgroundColor = "#CACACA";
+        }
+
+      });
+
       this.modo = this.tipoModo.DIA;
+
     }else{
+
       document.documentElement.style.colorScheme = "dark";
+
+      document.querySelectorAll("details").forEach(detail => {
+        if(detail instanceof HTMLDetailsElement){
+          detail.style.backgroundColor = "#262626";
+        }
+      });
+
+      document.querySelectorAll(".summary-chevron-down, .summary-chevron-up").forEach(flecha => {
+
+        if(flecha instanceof HTMLElement){
+          flecha.style.backgroundColor = "#262626";
+        }
+
+      });
+
       this.modo = this.tipoModo.NOCHE;
     }
 

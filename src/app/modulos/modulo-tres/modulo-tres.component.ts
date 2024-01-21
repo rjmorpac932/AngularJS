84c8068da17285e2323d5d2b19248b3e671be1dd
jmarcan286 <jmarcan286@iesalmudeyne.es>
2024-01-21 12:13:29 +0100

    [:rocket:] Modulos 1,2,3 con Clipboards Incluidos

--- src/app/modulos/modulo-tres/modulo-tres.component.ts
+++ src/app/modulos/modulo-tres/modulo-tres.component.ts
@@ -9,5 +9,18 @@ import { ClipboardComponent } from '../../clipboard/clipboard.component';
   styleUrl: '../modulo.css'
 })
 export class ModuloTresComponent {
-
+ /* NO INDENTAR */
+cNuevoProyecto = `ng new ProyectoAngular`;
+cNpmInstall = `npm install`;
+cNgServe = `ng serve`;
+cNgBuild = `ng build`;
+cNgTest = `ng test`;
+cNgEndToEnd = `ng e2e`;
+cNgGenerate =
+`ng generate component nombre-del-componente
+ng g service nombre-del-servicio
+ng g module nombre-del-módulo`;
+cNgLint = `ng lint`;
+cNgGenerateService = `ng generate service services/servicio`;
+cNgGenerateModule =`ng generate module nombre-del-módulo`;
 }

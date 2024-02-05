7d50663427598638a0fdfb15a040bfed7a5b4932
David <ddorluc760@iesalmudeyne.es>
2024-02-05 13:14:44 +0100

    Funcionalidad boton eliminar usuario

--- AngularProyectoBack/src/main/java/com/daw/controller/ApplicationController.java
+++ AngularProyectoBack/src/main/java/com/daw/controller/ApplicationController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +32,11 @@ public class ApplicationController {
 	public ResponseEntity<Usuario> busquedaUsuarioById(@PathVariable("id") Long id){
 		return ResponseEntity.ok().body(servicio.busquedaUsuarioById(id));
 	}
+	
+	@DeleteMapping("/usuarios/{id}")
+	public ResponseEntity<Usuario> eliminacionUsuarioEspecifico(@PathVariable("id") Long idUsuario){
+		return ResponseEntity.ok().body(servicio.eliminacionUsuarioEspecifico(idUsuario));
+	}
+	
 	
 }
--- AngularProyectoBack/src/main/java/com/daw/service/Servicio.java
+++ AngularProyectoBack/src/main/java/com/daw/service/Servicio.java
@@ -1,6 +1,7 @@
 package com.daw.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,6 +24,18 @@ public class Servicio {
 	
 	public Usuario busquedaUsuarioById(Long id) {
 		return usuarioRepository.getReferenceById(id);
+	}
+	
+	public Usuario eliminacionUsuarioEspecifico(Long idUsuario) {
+		Optional<Usuario> usuario = usuarioRepository.findById(idUsuario);
+		if(usuario.isPresent()) {
+			Usuario u = usuario.get();
+			usuarioRepository.delete(u);
+			return null;
+		}else {
+			return null;
+		}
+		
 	}
 	
 }

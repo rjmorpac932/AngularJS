cfbde1873f973b6e696bb34074dfd7dd0ba50ecf
jmarcan286 <jmarcan286@iesalmudeyne.es>
2024-02-11 20:17:50 +0100

    Merge de todas las ramas para el proyecto grupal

--- AngularProyectoBack/src/main/java/com/daw/controller/ApplicationController.java
+++ AngularProyectoBack/src/main/java/com/daw/controller/ApplicationController.java
@@ -9,11 +9,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.PostMapping;
-=======
 import org.springframework.web.bind.annotation.PutMapping;
->>>>>>> c1bf42f8e27d7e9e3dd6e8ee69d49566ed143a18
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,22 +28,16 @@ public class ApplicationController {
 	private Servicio servicio;
 
 	@GetMapping("/usuarios")
-<<<<<<< HEAD
-	public ResponseEntity<List<Usuario>> busquedaUsuarioGeneral() {
-		return ResponseEntity.ok().body(servicio.busquedaUsuarioGeneral());
-=======
 	public ResponseEntity<List<Usuario>> busquedaUsuarioGeneral(
 			@RequestParam(name = "usuario", required = false) String usuario,
 			@RequestParam(name = "nombreCompleto", required = false) String nombreCompleto) {
 		return ResponseEntity.ok().body(servicio.busquedaUsuarioGeneral(usuario, nombreCompleto));
->>>>>>> c1bf42f8e27d7e9e3dd6e8ee69d49566ed143a18
 	}
 
 	@GetMapping("/usuarios/{id}")
 	public ResponseEntity<Usuario> busquedaUsuarioById(@PathVariable("id") Long id) {
 		return ResponseEntity.ok().body(servicio.busquedaUsuarioById(id));
 	}
-<<<<<<< HEAD
 	
 	@DeleteMapping("/usuarios")
 	public void eliminarUsuarios(){
@@ -69,11 +60,9 @@ public class ApplicationController {
 		return servicio.crearUsuario(usuario);
 	}
 
-=======
 
 	@PutMapping("/actualizarUsuario/{id}")
 	public ResponseEntity<Usuario> modificarUsuario(@PathVariable("id") Long id, @RequestBody Usuario usuarioActualizado){
 		return ResponseEntity.ok().body(servicio.modificarUsuario(id, usuarioActualizado));
 	}
->>>>>>> c1bf42f8e27d7e9e3dd6e8ee69d49566ed143a18
 }
